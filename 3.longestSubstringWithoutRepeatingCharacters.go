5bca30415b676b8b4bf2a03e6fca7ed744720e6f
wojucai <2582504569@qq.com>
2024-04-28 00:38:56 +0800

    提交三个算法题

---
+++ 3.longestSubstringWithoutRepeatingCharacters.go
@@ -0,0 +1,26 @@
+package leetcode
+
+// 位图
+func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	var bitSet [256]bool
+	result, left, right := 0, 0, 0
+	for left < len(s) {
+		if bitSet[s[right]] {
+			bitSet[s[left]] = false
+			left++
+		} else {
+			bitSet[s[right]] = true
+			right++
+		}
+		if result < right-left {
+			result = right - left
+		}
+		if left+result >= len(s) || right >= len(s) {
+			break
+		}
+	}
+	return result
+}
